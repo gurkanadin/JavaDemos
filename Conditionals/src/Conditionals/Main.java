f71515f36001f3c164cc47d00280a6f8da5004a9
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-09-24 14:43:46 +0300

    Conditionals

---
+++ Conditionals/src/Conditionals/Main.java
@@ -0,0 +1,19 @@
+package Conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 20;
+		
+		if(number < 20) {
+			System.out.println("The number is lower than 20");
+		}else if(number == 20) {
+			System.out.println("The number is equal 20");
+		}
+		else {
+			System.out.println("The number is higher than 20");
+		}
+
+	}
+
+}
