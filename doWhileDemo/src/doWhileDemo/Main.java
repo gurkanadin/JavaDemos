e5ae49858714a2491996b15779ca9bf744d7bef6
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-09-24 15:46:13 +0300

    Do While Demo

---
+++ doWhileDemo/src/doWhileDemo/Main.java
@@ -0,0 +1,15 @@
+package doWhileDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int j = 1;
+		do {
+			System.out.println(j);
+			j += 2;
+		
+		}while(j<10);
+
+	}
+
+}
