267e4acdd56baf282aadeb781cf7afb409b32898
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-09-24 14:32:22 +0300

    Data Types

---
+++ DataTypes/src/datatypes/Main.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int firstNumber = 15;
+		
+		double number = 12.5;
+		number = -129;
+		
+		char character = 'G';
+		boolean isTrue = false;
+		
+				
+		
+	}
+
+}
