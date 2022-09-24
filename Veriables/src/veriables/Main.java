9b0ae85d52d312688ea229fa3e32832e556153dc
gurka <gurka@MonsterGurkan>
2022-09-24 12:12:36 +0300

    try

--- Veriables/src/veriables/Main.java
+++ Veriables/src/veriables/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int studentCounter = 12;
-		String message = "Öğrenci Sayısı :";
+		String message = "Öğrenci Sayısı : ";
 		System.out.println(message + studentCounter );
 
 	}
