d504a5b9b4ca390807ca10a7d3343c394ea6542b
gurka <gurka@MonsterGurkan>
2022-09-24 12:00:08 +0300

    First commit

---
+++ Veriables/src/veriables/Main.java
@@ -0,0 +1,12 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int studentCounter = 12;
+		String message = "Öğrenci Sayısı :";
+		System.out.println(message + studentCounter );
+
+	}
+
+}
