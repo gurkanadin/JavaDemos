40d7f53355e968a2557fe9785261683579167d20
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-09-24 15:40:04 +0300

    While Loop Demo

---
+++ loopWhileDemo/src/loopWhileDemo/Main.java
@@ -0,0 +1,16 @@
+package loopWhileDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//While
+		int i= 1;
+		
+		while(i<=10) {
+			System.out.println("Step " + i);
+			i++;
+		}
+
+	}
+
+}
