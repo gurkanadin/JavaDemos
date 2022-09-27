b426c289c9df80ab208d14137188b22134637572
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-09-27 17:06:18 +0300

    Vowels Project

---
+++ LettersProject/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char letter ='I';
+		
+		switch (letter){
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+		
+			
+			System.out.println("Thick vowels");
+			break;
+		default:
+			System.out.println("Thin vowels");
+		
+		}
+
+	}
+
+}
