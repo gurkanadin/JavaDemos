77ed18333af4621103042fb9d6d5807480f016ce
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-09-28 04:30:29 +0300

    Veriable Arguments

--- methods2/src/methods2/Main.java
+++ methods2/src/methods2/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		String message = "Good Job";
 		System.out.println(insert(message));
 		System.out.println(total(13, 7));
+		int number = totalNumber(2, 3, 4, 5, 6, 7, 8, 7, 8, 7, 8);
+		System.out.println(number);
 
 	}
 
@@ -15,6 +17,14 @@ public class Main {
 
 	public static int total(int number1, int number2) {
 		return number1 + number2;
+	}
+
+	public static int totalNumber(int... numbers) {
+		int totalNumbers = 0;
+		for (int number : numbers) {
+			totalNumbers += number;
+		}
+		return totalNumbers;
 	}
 
 }
