dd178b4a4fa84d2e3ed6354d0ae7d3272900ab10
gurkanadin <35859143+gurkanadin@users.noreply.github.com>
2022-10-28 22:57:41 +0300

    Static Demo

---
+++ staticDemo/src/staticDemo/DataBaseHelper.java
@@ -0,0 +1,23 @@
+package staticDemo;
+
+public class DataBaseHelper {
+	public static class Crud {
+		// CRUD = Create read update delete
+		public static void Delete() {
+
+		}
+
+		public static void Update() {
+
+		}
+		
+		
+		
+	}
+	public static class Connection{
+		public static void createConnection() {
+			
+		}
+	}
+
+}
---
+++ staticDemo/src/staticDemo/ProductManager.java
@@ -0,0 +1,14 @@
+package staticDemo;
+
+public class ProductManager {
+	public void add(Product product) {
+		
+		if (ProductValidator.isValid(product)) {
+			System.out.println("Added");
+		}else {
+			System.out.println("Invalid ");
+		}
+	}
+	
+
+}
